cbb6c82465146cf25385d438f374914e4e2aef5c
swalih_moosa <swalih@steyp.com>
2024-01-10 20:38:05 +0530

    landing-page updated

--- src/framework/basic-rest/category/get-all-categories.tsx
+++ src/framework/basic-rest/category/get-all-categories.tsx
@@ -1,39 +1,39 @@
-import { CategoriesQueryOptionsType, Category } from '@framework/types';
-import http from '@framework/utils/http';
-import { API_ENDPOINTS } from '@framework/utils/api-endpoints';
-import { useQuery } from '@tanstack/react-query';
+import { CategoriesQueryOptionsType, Category } from "@framework/types";
+import http from "@framework/utils/http";
+import { API_ENDPOINTS } from "@framework/utils/api-endpoints";
+import { useQuery } from "@tanstack/react-query";
 
 export const fetchCategories = async () => {
-  const {
-    data: { data },
-  } = await http.get(API_ENDPOINTS.CATEGORIES);
-  return {
-    categories: {
-      data: data as Category[],
-    },
-  };
+    const {
+        data: { data },
+    } = await http.get(API_ENDPOINTS.CATEGORIES);
+    return {
+        categories: {
+            data: data as Category[],
+        },
+    };
 };
 
 const fetchAncientCategories = async () => {
-  const {
-    data: { data },
-  } = await http.get(API_ENDPOINTS.CATEGORIES_ANCIENT);
-  return {
-    categories: {
-      data: data as Category[],
-    },
-  };
+    const {
+        data: { data },
+    } = await http.get(API_ENDPOINTS.CATEGORIES_ANCIENT);
+    return {
+        categories: {
+            data: data as Category[],
+        },
+    };
 };
 
 export const useCategoriesQuery = (options: CategoriesQueryOptionsType) => {
-  if (options.demoVariant === 'ancient') {
+    if (options.demoVariant === "ancient") {
+        return useQuery<{ categories: { data: Category[] } }, Error>({
+            queryKey: [API_ENDPOINTS.CATEGORIES, options],
+            queryFn: fetchAncientCategories,
+        });
+    }
     return useQuery<{ categories: { data: Category[] } }, Error>({
-      queryKey: [API_ENDPOINTS.CATEGORIES, options],
-      queryFn: fetchAncientCategories
+        queryKey: [API_ENDPOINTS.CATEGORIES, options],
+        queryFn: fetchCategories,
     });
-  }
-  return useQuery<{ categories: { data: Category[] } }, Error>({
-    queryKey: [API_ENDPOINTS.CATEGORIES, options],
-    queryFn: fetchCategories
-  });
 };
