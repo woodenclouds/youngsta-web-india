00c96ec037cec30d99f03d75a9aaff03f727e886
Ahammad <apple@APPLEs-MacBook-Air-2.local>
2024-01-12 18:14:21 +0530

    og image

--- src/components/layout/layout-two.tsx
+++ src/components/layout/layout-two.tsx
@@ -20,12 +20,12 @@ export default function Layout({ children }: React.PropsWithChildren<{}>) {
 						content: 'width=device-width, initial-scale=1.0',
 					},
 				]}
-				title="ChawkBazar React - React Next E-commerce Template"
+				title="Youngsta React - React Next E-commerce Template"
 				description="Fastest E-commerce template built with React, NextJS, TypeScript, @tanstack/react-query and Tailwind CSS."
-				canonical="https://chawkbazar.vercel.app/"
+				canonical="https://youngsta.vercel.app/"
 				openGraph={{
-					url: 'https://chawkbazar.vercel.app',
-					title: 'ChawkBazar React - React Next E-commerce Template',
+					url: 'https://youngsta.vercel.app/',
+					title: 'Youngsta React - React Next E-commerce Template',
 					description:
 						'Fastest E-commerce template built with React, NextJS, TypeScript, @tanstack/react-query and Tailwind CSS.',
 					images: [
