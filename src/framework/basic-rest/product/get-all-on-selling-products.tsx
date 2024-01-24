6bf2becfb05e6a11ea1faccfa4f7efc8f4597c78
swalih_moosa <swalih@steyp.com>
2024-01-24 21:36:04 +0530

    updates ongoing

--- src/framework/basic-rest/product/get-all-on-selling-products.tsx
+++ src/framework/basic-rest/product/get-all-on-selling-products.tsx
@@ -4,18 +4,22 @@ import { API_ENDPOINTS } from "@framework/utils/api-endpoints";
 import { useQuery } from "@tanstack/react-query";
 
 export const fetchOnSellingProducts = async () => {
-  const {
-    data: {
-        app_data: { data },
-    },
-  } = await http.get(API_ENDPOINTS.ON_SELLING_PRODUCTS);
-  return data;
+    const {
+        data: {
+            app_data: { data, status },
+        },
+    } = await http.get(API_ENDPOINTS.ON_SELLING_PRODUCTS);
 
+    if (status === 6000) {
+        return data;
+    } else {
+        return [];
+    }
 };
 
 export const useOnSellingProductsQuery = (options: QueryOptionsType) => {
-  return useQuery<Product[], Error>({
-    queryKey: [API_ENDPOINTS.ON_SELLING_PRODUCTS, options],
-    queryFn: fetchOnSellingProducts
-  });
+    return useQuery<Product[], Error>({
+        queryKey: [API_ENDPOINTS.ON_SELLING_PRODUCTS, options],
+        queryFn: fetchOnSellingProducts,
+    });
 };
