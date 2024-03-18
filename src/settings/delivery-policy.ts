bd966e26b5956eeafbe89cd1c4599fecbf8f293a
swalih_moosa <swalih@steyp.com>
2024-03-18 09:48:59 +0530

    contents updated

---
+++ src/settings/delivery-policy.ts
@@ -0,0 +1,8 @@
+export const deliveryPolicy = [
+    {
+        id: "1",
+        title: "Shipping & Delivery Policy",
+        description:
+            "<p>For International buyers, orders are shipped and delivered through registered international courier companies and/or International speed post only. For domestic buyers, orders are shipped through registered domestic courier companies and /or speed post only. Orders are shipped within 3-5 Days days or as per the delivery date agreed at the time of order confirmation and delivering of the shipment subject to Courier Company / post office norms.<br />ARUN A V is not liable for any delay in delivery by the courier company / postal authorities and only guarantees to hand over the consignment to the courier company or postal authorities within 3-5 Days days from the date of the order and payment or as per the delivery date agreed at the time of order confirmation. Delivery of all orders will be to the address provided by the buyer. Delivery of our services will be confirmed on your mail ID as specified during registration. For any issues in utilizing our services you may contact our helpdesk on 7306169756 or youngstatech@gmail.com.</p>",
+    },
+];
--- src/settings/privacy-settings.ts
+++ src/settings/privacy-settings.ts
@@ -1,32 +1,47 @@
 export const privacyPolicy = [
-  {
-    id: "1",
-    title: "privacy-one-title",
-    description:"privacy-one-content",
-  },
-  {
-    id: "2",
-    title: "privacy-two-title",
-    description: "privacy-two-content",
-  },
-  {
-    id: "3",
-    title: "privacy-three-title",
-    description:"privacy-three-content",
-  },
-  {
-    id: "4",
-    title: "privacy-four-title",
-    description:"privacy-four-content",
-  },
-  {
-    id: "5",
-    title: "privacy-five-title",
-    description:"privacy-five-content",
-  },
-  {
-    id: "6",
-    title: "privacy-six-title",
-    description:"privacy-six-content",
-  },
+    {
+        id: "1",
+        title: "privacy-one-title",
+        description: "privacy-one-content",
+    },
+    {
+        id: "2",
+        title: "privacy-two-title",
+        description: "privacy-two-content",
+    },
+    {
+        id: "3",
+        title: "privacy-three-title",
+        description: "privacy-three-content",
+    },
+    {
+        id: "4",
+        title: "privacy-four-title",
+        description: "privacy-four-content",
+    },
+    {
+        id: "5",
+        title: "privacy-five-title",
+        description: "privacy-five-content",
+    },
+    {
+        id: "6",
+        title: "privacy-six-title",
+        description: "privacy-six-content",
+    },
+    {
+        id: "7",
+        title: "privacy-seven-title",
+        description: "privacy-seven-content",
+    },
+    {
+        id: "8",
+        title: "privacy-eight-title",
+        description: "privacy-eight-content",
+    },
+    {
+        id: "9",
+        title: "privacy-nine-title",
+        description: "privacy-nine-content",
+    },
 ];
