6686f4f183bb5c2f5f30deb964c1ae68553c032a
swalih_moosa <techniatech@gmail.com>
2024-04-21 19:36:39 +0530

    _

--- src/framework/basic-rest/utils/api-endpoints.ts
+++ src/framework/basic-rest/utils/api-endpoints.ts
@@ -1,21 +1,7 @@
 export const API_ENDPOINTS = {
     FORGET_PASSWORD: "/forget-password",
-    CATEGORIES_2: "/categories.json",
-    FEATURED_CATEGORIES: "/featured_categories.json",
-    COLLECTIONS: "/collections.json",
-    BRANDS: "/brands.json",
-    BRANDS_ANCIENT: "/brands_ancient.json",
-    PRODUCTS_2: "/products_2.json",
-    PRODUCTS_ANCIENT: "/products_ancient.json",
-    FEATURED_PRODUCTS_ANCIENT: "/featured_products_ancient.json",
-    TOP_SELLER_PRODUCTS: "/products_top_seller.json",
     ON_SELLING_PRODUCTS: "products/viewproduct/?tags=on-selling-products",
-    PRODUCT: "/product.json",
-    BEST_SELLER_PRODUCTS: "/products_best_seller.json",
     NEW_ARRIVAL_PRODUCTS: "products/viewproduct/?tags=new-arrival-products",
-    POPULAR_PRODUCTS: "/products_popular.json",
-    NEW_ARRIVAL_PRODUCTS_ANCIENT: "/products_new_arrival.json",
-    FLASH_SALE_PRODUCTS_ANCIENT: "/products_flash_sale_ancient.json",
 
     // account endpoints
     VIEW_ADDRESS: "accounts/view_addresses/",
