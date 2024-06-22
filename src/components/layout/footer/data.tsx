bf26259aa21ac878daba379ed9b08f78852126b6
jismy12 <jismy.woodenclouds@gmail.com>
2024-06-22 17:58:48 +0530

    bugg fixing

--- src/components/layout/footer/data.tsx
+++ src/components/layout/footer/data.tsx
@@ -9,6 +9,103 @@ export const footer = {
     widgets: [
         {
             id: 1,
+            widgetTitle: "Company",
+            lists: [
+                {
+                    id: 1,
+                    title: "link-contact-us",
+                    path: "/contact-us",
+                },
+                {
+                    id: 2,
+                    title: "link-email",
+                    path: "/",
+                },
+                {
+                    id: 3,
+                    title: "link-email-two",
+                    path: "/",
+                },
+                {
+                    id: 4,
+                    title: "link-phone",
+                    path: "/",
+                },
+            ],
+        },
+        {
+            id: 2,
+            widgetTitle: "Policies",
+            lists: [
+                {
+                    id: 1,
+                    title: "link-privacy",
+                    path: "/privacy",
+                },
+                {
+                    id: 2,
+                    title: "link-terms",
+                    path: "/terms",
+                },
+                {
+                    id: 3,
+                    title: "link-return-policy",
+                    path: "/refund-policy",
+                },
+                {
+                    id: 4,
+                    title: "link-site-map",
+                    path: "/delivery-policy",
+                },
+            ],
+        },
+        
+        {
+            id: 4,
+            widgetTitle: "My-Account",
+            lists: [
+                
+                {
+                    id: 1,
+                    title: "Login",
+                    path: "/",
+                },
+                {
+                    id: 2,
+                    title: "Register",
+                    path: "/",
+                },
+            ],
+        },
+        
+        {
+            id: 5,
+            widgetTitle: "widget-title-top-categories",
+            lists: [
+                {
+                    id: 1,
+                    title: "link-men-wear",
+                    path: "/search",
+                },
+                {
+                    id: 2,
+                    title: "link-men-wear",
+                    path: "/search",
+                },
+                {
+                    id: 3,
+                    title: "link-kids-wear",
+                    path: "/search",
+                },
+                {
+                    id: 4,
+                    title: "link-sports-wear",
+                    path: "/search",
+                },
+            ],
+        },
+        {
+            id: 6,
             widgetTitle: "widget-title-social",
             lists: [
                 {
@@ -37,131 +134,9 @@ export const footer = {
                 },
             ],
         },
-        {
-            id: 2,
-            widgetTitle: "widget-title-contact",
-            lists: [
-                {
-                    id: 1,
-                    title: "link-contact-us",
-                    path: "/contact-us",
-                },
-                {
-                    id: 2,
-                    title: "link-email",
-                    path: "/",
-                },
-                {
-                    id: 3,
-                    title: "link-email-two",
-                    path: "/",
-                },
-                {
-                    id: 4,
-                    title: "link-phone",
-                    path: "/",
-                },
-            ],
-        },
-        {
-            id: 3,
-            widgetTitle: "widget-title-about",
-            lists: [
-                {
-                    id: 1,
-                    title: "link-support-center",
-                    path: "/contact-us",
-                },
-                {
-                    id: 2,
-                    title: "link-customer-support",
-                    path: "/",
-                },
-                {
-                    id: 3,
-                    title: "link-about-us",
-                    path: "/contact-us",
-                },
-                {
-                    id: 4,
-                    title: "link-copyright",
-                    path: "/",
-                },
-            ],
-        },
-        {
-            id: 4,
-            widgetTitle: "widget-title-customer-care",
-            lists: [
-                {
-                    id: 1,
-                    title: "link-faq",
-                    path: "/faq",
-                },
-                {
-                    id: 2,
-                    title: "link-shipping",
-                    path: "/",
-                },
-                {
-                    id: 3,
-                    title: "link-exchanges",
-                    path: "/",
-                },
-            ],
-        },
-        {
-            id: 5,
-            widgetTitle: "widget-title-our-information",
-            lists: [
-                {
-                    id: 1,
-                    title: "link-privacy",
-                    path: "/privacy",
-                },
-                {
-                    id: 2,
-                    title: "link-terms",
-                    path: "/terms",
-                },
-                {
-                    id: 3,
-                    title: "link-return-policy",
-                    path: "/refund-policy",
-                },
-                {
-                    id: 4,
-                    title: "link-site-map",
-                    path: "/delivery-policy",
-                },
-            ],
-        },
-        {
-            id: 6,
-            widgetTitle: "widget-title-top-categories",
-            lists: [
-                {
-                    id: 1,
-                    title: "link-men-wear",
-                    path: "/search",
-                },
-                {
-                    id: 2,
-                    title: "link-men-wear",
-                    path: "/search",
-                },
-                {
-                    id: 3,
-                    title: "link-kids-wear",
-                    path: "/search",
-                },
-                {
-                    id: 4,
-                    title: "link-sports-wear",
-                    path: "/search",
-                },
-            ],
-        },
+
+
+        
     ],
     payment: [
         {
--- src/components/layout/footer/widgets.tsx
+++ src/components/layout/footer/widgets.tsx
@@ -21,8 +21,8 @@ const Widgets: React.FC<WidgetsProps> = ({ widgets, variant }) => {
         className={cn(
           'grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5 md:gap-9 xl:gap-5  pb-9 md:pb-14 lg:pb-16 2xl:pb-20 3xl:pb-24 lg:mb-0.5 2xl:mb-0 3xl:-mb-1',
           {
-            'xl:grid-cols-6': variant !== 'contemporary',
-            'xl:grid-cols-7': variant === 'contemporary',
+            'xl:grid-cols-5': variant !== 'contemporary',
+            'xl:grid-cols-6': variant === 'contemporary',
           }
         )}
       >
