7c66990a5130292656c708ce48649ac9b1c55c75
Wazeer Ahmed <wazeer@hashinclude.io>
2024-06-24 17:26:37 +0530

    fix:footer route issue

--- src/components/widgets/widget-link.tsx
+++ src/components/widgets/widget-link.tsx
@@ -1,8 +1,9 @@
-import type { FC } from "react";
+import React, { useEffect, type FC } from "react";
 import Link from "next/link";
 import { useTranslation } from "next-i18next";
 import Logo from "@components/ui/logo";
 import { useUI } from "@contexts/ui.context";
+import Cookies from "js-cookie";
 
 interface Props {
   className?: string;
@@ -26,6 +27,14 @@ const WidgetLink: FC<Props> = ({ className, data }) => {
   const { logo, description } = data;
   const { t } = useTranslation("footer");
   const { isAuthorized, setModalView, openModal } = useUI();
+  const [accessToken, setAccessToken] = React.useState<string | null>(null);
+  const token = Cookies.get("auth_token");
+
+  useEffect(() => {
+    if (token) {
+      setAccessToken(token);
+    }
+  }, [token]);
   return (
     <div
       className={`${className} ${data?.isCompanyIntroduction && "col-span-2"}`}
@@ -36,7 +45,7 @@ const WidgetLink: FC<Props> = ({ className, data }) => {
             {t(`${widgetTitle}`)}
           </h4>
           <ul className="list-none text-xs lg:text-sm text-body flex flex-col space-y-3 lg:space-y-3.5">
-            {lists.map((list) => (
+            {lists.map((list, index) => (
               <li
                 key={`widget-list--key${list.id}`}
                 className="flex items-baseline"
@@ -47,11 +56,27 @@ const WidgetLink: FC<Props> = ({ className, data }) => {
                   </span>
                 )}
                 {list.title === "Login" || list.title === "Register" ? (
-                  <span className="cursor-pointer transition-colors duration-200 hover:text-black" onClick={() => { setModalView("LOGIN_VIEW"); openModal(); }}>{t(`${list.title}`)}</span>
+                  token ? (
+                    <Link href="/my-account">
+                      <a className={`${index == 1 ? "hidden" : ""}`}>
+                        My Account
+                      </a>
+                    </Link>
+                  ) : (
+                    <span
+                      className="cursor-pointer transition-colors duration-200 hover:text-black"
+                      onClick={() => {
+                        setModalView("LOGIN_VIEW");
+                        openModal();
+                      }}
+                    >
+                      {t(`${list.title}`)}
+                    </span>
+                  )
                 ) : (
                   <Link href={list.path ? list.path : "#!"}>
                     <a className="transition-colors duration-200 hover:text-black">
-                    {t(`${list.title}`)}
+                      {t(`${list.title}`)}
                     </a>
                   </Link>
                 )}
