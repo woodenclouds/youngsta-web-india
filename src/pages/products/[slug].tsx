4140e795242d78869e04903e72dfbd419838ac65
Wazeer Ahmed <wazeer@hashinclude.io>
2024-06-27 10:04:40 +0530

    fix:metadata updated

--- src/pages/products/[slug].tsx
+++ src/pages/products/[slug].tsx
@@ -6,33 +6,59 @@ import Divider from "@components/ui/divider";
 import Breadcrumb from "@components/common/breadcrumb";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 import { GetServerSideProps } from "next";
+import { useProductQuery } from "@framework/product/get-product";
+import { useRouter } from "next/router";
+import Head from "next/head";
 
 export default function ProductPage() {
-    return (
-        <>
-            <Divider className="mb-0" />
-            <Container>
-                <div className="pt-8">
-                    <Breadcrumb />
-                </div>
-                <ProductSingleDetails />
-                <RelatedProducts sectionHeading="text-related-products" />
-            </Container>
-        </>
-    );
+  const {
+    query: { slug, refferal_code },
+  } = useRouter();
+  const { data, isLoading } = useProductQuery(slug as string);
+
+  const meta = {
+    title: "",
+    description: "",
+    image: "",
+  };
+
+  if (data) {
+    meta.title = data.name;
+    meta.description = data.description;
+    meta.image = data.thumbnail;
+  }
+
+  console.log(data, "_log_data_");
+
+  return (
+    <>
+      <Head>
+        <title>{meta.title}</title>
+        <meta name="description" content={meta.description} />
+      </Head>
+      <Divider className="mb-0" />
+      <Container>
+        <div className="pt-8">
+          <Breadcrumb />
+        </div>
+        <ProductSingleDetails />
+        <RelatedProducts sectionHeading="text-related-products" />
+      </Container>
+    </>
+  );
 }
 
 ProductPage.Layout = Layout;
 
 export const getServerSideProps: GetServerSideProps = async ({ locale }) => {
-    return {
-        props: {
-            ...(await serverSideTranslations(locale!, [
-                "common",
-                "forms",
-                "menu",
-                "footer",
-            ])),
-        },
-    };
+  return {
+    props: {
+      ...(await serverSideTranslations(locale!, [
+        "common",
+        "forms",
+        "menu",
+        "footer",
+      ])),
+    },
+  };
 };
