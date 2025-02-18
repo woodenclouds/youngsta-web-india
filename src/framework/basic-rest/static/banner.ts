0285a709c31758d3f20b050c863c5e4d28e9da71
safwanwc <safwan.woodenclouds@gmail.com>
2025-02-18 10:25:57 +0530

    Update hero banner images and comment out unused banner data

--- src/framework/basic-rest/static/banner.ts
+++ src/framework/basic-rest/static/banner.ts
@@ -23,51 +23,53 @@ export const homeOneHeroBanner = [
         slug: "winter-collection",
         image: {
             mobile: {
-                url: "/assets/images/hero/banner-mobile-1.jpg",
+                // url: "/assets/images/hero/banner-mobile-1.jpg",
+                url: "/assets/images/banner/slider/mobile/banner-new-1.png",
                 width: 480,
                 height: 275,
             },
             desktop: {
-                url: "/assets/images/hero/banner-1.jpg",
+                // url: "/assets/images/hero/banner-1.jpg",
+                url: "/assets/images/banner/slider/banner-new-1.png",
                 width: 1800,
                 height: 800,
             },
         },
     },
-    {
-        id: 2,
-        title: "gift collection",
-        slug: "gift-collection",
-        image: {
-            mobile: {
-                url: "/assets/images/hero/banner-mobile-2.jpg",
-                width: 480,
-                height: 275,
-            },
-            desktop: {
-                url: "/assets/images/hero/banner-2.jpg",
-                width: 1800,
-                height: 800,
-            },
-        },
-    },
-    {
-        id: 3,
-        title: "party collection",
-        slug: "party-collection",
-        image: {
-            mobile: {
-                url: "/assets/images/hero/banner-mobile-3.jpg",
-                width: 480,
-                height: 275,
-            },
-            desktop: {
-                url: "/assets/images/hero/banner-3.jpg",
-                width: 1800,
-                height: 800,
-            },
-        },
-    },
+    // {
+    //     id: 2,
+    //     title: "gift collection",
+    //     slug: "gift-collection",
+    //     image: {
+    //         mobile: {
+    //             url: "/assets/images/hero/banner-mobile-2.jpg",
+    //             width: 480,
+    //             height: 275,
+    //         },
+    //         desktop: {
+    //             url: "/assets/images/hero/banner-2.jpg",
+    //             width: 1800,
+    //             height: 800,
+    //         },
+    //     },
+    // },
+    // {
+    //     id: 3,
+    //     title: "party collection",
+    //     slug: "party-collection",
+    //     image: {
+    //         mobile: {
+    //             url: "/assets/images/hero/banner-mobile-3.jpg",
+    //             width: 480,
+    //             height: 275,
+    //         },
+    //         desktop: {
+    //             url: "/assets/images/hero/banner-3.jpg",
+    //             width: 1800,
+    //             height: 800,
+    //         },
+    //     },
+    // },
 ];
 
 export const homeTwoHeroBanner = [
